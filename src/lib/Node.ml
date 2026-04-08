960b204e44ece9391bc58bf1a3be1fc08c908afa
Codevibes1234 <venkatkoundinya.s@gmail.com>
2026-04-08 13:11:16 +0530

    Added getters and setters

--- src/lib/Node.ml
+++ src/lib/Node.ml
@@ -1,15 +1,13 @@
 
-type t =
-  Node :
+type ('a,'b) t =
   {
-    decide_next : t CASConsensus.t;
-    mutable next : t option;
+    decide_next : 'a CASConsensus.t;
+    mutable next : ('a,'b) t option;
     mutable seq : int;
-    invoc : 'f; (*invoc is a function*)
+    invoc : 'b -> 'b (*invoc is a function*)
   }
-  -> t
 
-let create invoc num_threads = Node {
+let create invoc num_threads = {
     decide_next = CASConsensus.create num_threads; 
     next = None; 
     seq = 0;
@@ -19,11 +17,21 @@ let create invoc num_threads = Node {
 let max arr = 
   let rec aux acc i = 
     if i >= Array.length arr then acc
-    else begin match arr.(i) with
-    | Node r -> begin match acc with
-                | Node acc_r -> if r.seq > acc_r.seq then aux arr.(i) (i + 1) 
-                                else aux acc (i + 1)
-                end
-    end
+    else if arr.(i).seq > acc.seq then aux arr.(i) (i+1)
+    else aux acc (i+1)
   in
   aux arr.(0) 1
+
+let set_seq r v =
+  r.seq <- v
+
+let get_seq r = r.seq
+
+let get_next r = r.next
+
+let set_next r next_node =
+  r.next <- Some next_node
+
+let get_decide_next r = r.decide_next
+
+let get_invoc r = r.invoc
--- src/lib/Node.mli
+++ src/lib/Node.mli
@@ -1,9 +1,22 @@
 
-
-type t
+type ('a,'b) t
 
 (** Creates a new node with the given value and sequence number *)
-val create : 'a -> int -> t
+val create : ('b -> 'b) -> int -> ('a,'b) t
 
 (** Returns the node with the maximum sequence number in the array *)
-val max : t Array.t -> t
+val max : ('a,'b) t Array.t -> ('a,'b) t
+
+(** Updates the sequence number of the given node *)
+val set_seq : ('a,'b) t -> int -> unit
+
+val get_seq : ('a,'b) t -> int
+
+(** Returns the CAS consensus instance associated with the given node *)
+val get_decide_next : ('a,'b) t -> 'a CASConsensus.t
+
+val set_next : ('a,'b) t -> ('a,'b) t -> unit
+
+val get_next : ('a,'b) t -> ('a,'b) t option
+
+val get_invoc : ('a,'b) t -> ('b -> 'b)
