d5636b24bac6d2bc062cfa3a602ee0a66524f834
siddharthsg248 <siddharthsgeorge248@gmail.com>
2026-04-16 20:46:26 +0530

    add list/skip_list Lin specs

--- src/test/qcheck_lin_tests.ml
+++ src/test/qcheck_lin_tests.ml
@@ -64,3 +64,83 @@ module WFQueueTest = Lin_domain.Make(QueueSpec(WFQueue))
 
 
 
+module ListSpec (L : sig
+  type 'a t
+  val create : int -> 'a t
+  val apply : 'a t -> 'a Sequential.SequentialSortedList.op -> int -> 'a option
+end) = struct
+  type t = int L.t
+
+  let init () = L.create 4
+  let cleanup _ = ()
+
+  let api =
+    let open Lin in
+    [
+      val_ "insert"
+        (fun l x tid -> L.apply l (Sequential.SequentialSortedList.Insert x) tid)
+        (t @-> int_small @-> int @-> returning option int);
+
+      val_ "remove"
+        (fun l x tid -> L.apply l (Sequential.SequentialSortedList.Remove x) tid)
+        (t @-> int_small @-> int @-> returning option int);
+
+      val_ "contains"
+        (fun l x tid -> L.apply l (Sequential.SequentialSortedList.Contains x) tid)
+        (t @-> int_small @-> int @-> returning option int);
+    ]
+end
+
+module LFListTest = Lin_domain.Make(ListSpec(LFList))
+module WFListTest = Lin_domain.Make(ListSpec(WFList))
+
+
+
+
+module SkipListSpec (L : sig
+  type 'a t
+  val create : int -> 'a t
+  val apply : 'a t -> 'a Universal_instances.SeqSkipListAdapter.op -> int -> 'a option
+end) = struct
+  type t = int L.t
+
+  let init () = L.create 4
+  let cleanup _ = ()
+
+  let api =
+    let open Lin in
+    [
+      val_ "insert"
+        (fun l x tid -> L.apply l (Universal_instances.SeqSkipListAdapter.Insert x) tid)
+        (t @-> int_small @-> int @-> returning option int);
+
+      val_ "remove"
+        (fun l x tid -> L.apply l (Universal_instances.SeqSkipListAdapter.Remove x) tid)
+        (t @-> int_small @-> int @-> returning option int);
+
+      val_ "contains"
+        (fun l x tid -> L.apply l (Universal_instances.SeqSkipListAdapter.Contains x) tid)
+        (t @-> int_small @-> int @-> returning option int);
+    ]
+end
+
+module LFSkipListTest = Lin_domain.Make(SkipListSpec(LFSkipList))
+module WFSkipListTest = Lin_domain.Make(SkipListSpec(WFSkipList))
+
+
+
+let () =
+  QCheck_base_runner.run_tests_main [
+
+    LFStackTest.lin_test ~count:200 ~name:"LF Stack Lin";
+    WFStackTest.lin_test ~count:200 ~name:"WF Stack Lin";
+
+    LFQueueTest.lin_test ~count:200 ~name:"LF Queue Lin";
+    WFQueueTest.lin_test ~count:200 ~name:"WF Queue Lin";
+
+    LFListTest.lin_test ~count:200 ~name:"LF List Lin";
+    WFListTest.lin_test ~count:200 ~name:"WF List Lin";
+
+    LFSkipListTest.lin_test ~count:200 ~name:"LF SkipList Lin";
+    WFSkipListTest.lin_test ~count:200 ~name:"WF SkipList Lin";
+  ]
